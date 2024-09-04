4b22028b613014e29bc6e5df3c581531b4a89d3e
casas-javer <139288692+casas-javer@users.noreply.github.com>
2024-09-04 21:55:35 +0000

    ancho y menu

--- src/App.tsx
+++ src/App.tsx
@@ -52,16 +52,18 @@ function App() {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className='justify-center flex flex-row pt-4'>
-        <img className="d" width={200} src={logo} alt="Logo" />
 
-      </div>
-      <div className='justify-center flex flex-row py-4'>
-        <ModeToggle />
-      </div>
-      {/* <div className="card">
-        <Button variant={'secondary'} onClick={() => setCount((count) => count + 1)}>Click me {count} </Button>
-      </div> */}
+      <section className="bg-slate-100 dark:bg-slate-900 ">
+        <div className="container mx-auto p-4">
+          <div className="bg-slate-100 dark:bg-slate-900 flex items-center justify-center p-4 relative">
+            <img width={200} src={logo} alt="Logo" className="absolute left-1/2 transform -translate-x-1/2" />
+            <div className="ml-auto">
+              <ModeToggle />
+            </div>
+          </div>
+        </div>
+      </section>
+
 
 
 
@@ -73,17 +75,7 @@ function App() {
                 Crédito Fovissste
               </h1>
               <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Todo lo que necesitas saber sobre en nuevo esquema crediticio "FOVISSSTE para Todos" está en este vídeo. ¡Compartelo!</p>
-              {/* <div className="flex flex-col justify-center space-y-4 sm:flex-row sm:space-y-0">
-              <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                Contacto
-                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                </svg>
-              </a>
-              <a href="#" className="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                Más información
-              </a>
-            </div> */}
+
               <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                   <FormField
@@ -120,9 +112,13 @@ function App() {
 
 
             </div>
-            <div className=" text-white ">
-              <iframe className="mx-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl" src="https://www.youtube.com/embed/vri5jwbG7xo?si=wYvjmcl35JM-nDYC" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-
+            <div className="text-white">
+              <iframe
+                className="ml-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl"
+                src="https://www.youtube.com/embed/vri5jwbG7xo?si=wYvjmcl35JM-nDYC"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              ></iframe>
             </div>
           </div>
         </div>
